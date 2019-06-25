ce02a8c05c40ef8168e333fe6240700f68a9e8d0
user18 <user18@dypm.org>
2019-06-25 19:43:04 +0530

    first commit

---
+++ my_test.js
@@ -0,0 +1,17 @@
+var express=required('express');
+var app=expres();
+
+function add(a,b){
+	return a+b;
+};
+var addcontroller=function(request,response){
+	var result= add(10,20);
+	response.send("Addtion of 10 and 20 is "+result);
+	
+	
+};
+	app.get('/add',addcontroller);
+	app.listen(8080,function(){
+		console.log("runnig on 8080")
+		
+	})
